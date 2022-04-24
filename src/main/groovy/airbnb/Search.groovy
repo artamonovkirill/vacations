e11869e94bc5c915a5f3cc505455dbf9bf7b693a
Kirill Artamonov <artamonov.kirill@gmail.com>
2022-04-24 21:48:35 +0200

    :bug: update to new AirBnb layout

--- src/main/groovy/airbnb/Search.groovy
+++ src/main/groovy/airbnb/Search.groovy
@@ -11,9 +11,9 @@ class Search {
 
     static List<Listing> coordinates(URL search) {
         def html = xmlSlurper.parse(search as String)
-        def json = html.'**'.find { it.'@id' == 'data-state' }.text()
+        String json = html.'**'.find { it.'@id' == 'data-state' }.text()
         def data = jsonSlurper.parseText(json)
-        return data.niobeMinimalClientData[1][1].data.dora.exploreV3.sections*.items*.listing
+        return data.niobeMinimalClientData[1][1].data.presentation.explore.sections.sections*.section*.child*.section*.items*.listing
                 .flatten()
                 .findAll { it }
                 .collect { new Listing("https://www.airbnb.com/rooms/${it.id}".toURL(), it.lat, it.lng) }
