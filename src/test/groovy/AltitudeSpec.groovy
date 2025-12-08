f4209e4112eb153788d17395952a356a4d2997c2
Kirill Artamonov <artamonov.kirill@gmail.com>
2025-12-08 22:15:16 +0100

    :bug: Fix parsing
    - fix Airbnb parsing
    - add CI
    - migrate from spark to javalin
    - Groovy 5

--- src/test/groovy/AltitudeSpec.groovy
+++ src/test/groovy/AltitudeSpec.groovy
@@ -1,15 +1,20 @@
 import com.tomtom.http.HttpClient
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
 
 import static com.tomtom.http.response.ResponseCode.OK
-import static spark.Spark.awaitInitialization
 
 class AltitudeSpec extends Specification {
     def http = new HttpClient(baseUrl: 'http://localhost:4567')
 
     def setupSpec() {
         Vacations.main()
-        awaitInitialization()
+
+        def conditions = new PollingConditions(timeout: 5, initialDelay: 0.1, delay: 0.1)
+        conditions.eventually {
+            def response = new HttpClient(baseUrl: 'http://localhost:4567').get(path: '/health')
+            assert response.statusCode == OK
+        }
     }
 
     def 'finds all search altitudes'() {
@@ -26,15 +31,14 @@ class AltitudeSpec extends Specification {
         !response.body["results"].findAll { !it.lat || !it.lng || !it.url || !it.elevation }
 
         where:
-        url << ['https://www.airbnb.com/s/homes?ne_lat=72.38927453041205&ne_lng=38.463112574493756&sw_lat=29.270725469587948&sw_lng=-29.80311257449376&room_types%5B%5D=Entire%20home%2Fapt&property_type_id%5B%5D=2&search_type=NAVIGATION_CARD&title_type=CURATED_PROPERTY_TYPE',
-                'https://www.airbnb.com/s/Wengen--Switzerland/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&query=Wengen%2C%203823%20Lauterbrunnen%2C%20Switzerland&place_id=ChIJYdRtHsahj0cRgQMvzvqWqbE&date_picker_type=calendar&checkin=2022-07-16&checkout=2022-07-23&adults=3&children=2&source=structured_search_input_header&search_type=user_map_move&price_max=311&min_bedrooms=3&ne_lat=46.71360253305795&ne_lng=8.220160174618854&sw_lat=46.45404373470356&sw_lng=7.742254901181354&zoom=11&search_by_map=true']
+        url << ['https://www.airbnb.co.uk/s/Saas~Fee--Switzerland/homes?refinement_paths%5B%5D=%2Fhomes&date_picker_type=calendar&search_type=user_map_move&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2026-01-01&monthly_length=3&monthly_end_date=2026-04-01&price_filter_input_type=2&channel=EXPLORE&place_id=ChIJp2CD9qJFj0cRzho9eqWJgOU&acp_id=74e0fe62-22b2-4992-ab87-943645bf07b0&query=Saas-Fee%2C%20Switzerland&search_mode=regular_search&price_filter_num_nights=5&ne_lat=46.115691236231484&ne_lng=7.934395665391833&sw_lat=46.1111932114823&sw_lng=7.928865876837563&zoom=16.82584670940735&zoom_level=16.82584670940735&search_by_map=true&disable_auto_translation=true']
     }
 
     def 'finds search altitudes with minimum filter'() {
         when:
         def response = http.post(
                 path: '/altitudes?min=9000',
-                body: 'https://www.airbnb.com/s/Wengen--Switzerland/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&query=Wengen%2C%203823%20Lauterbrunnen%2C%20Switzerland&place_id=ChIJYdRtHsahj0cRgQMvzvqWqbE&date_picker_type=calendar&checkin=2022-07-16&checkout=2022-07-23&adults=3&children=2&source=structured_search_input_header&search_type=user_map_move&price_max=311&min_bedrooms=3&ne_lat=46.71360253305795&ne_lng=8.220160174618854&sw_lat=46.45404373470356&sw_lng=7.742254901181354&zoom=11&search_by_map=true',
+                body: 'https://www.airbnb.co.uk/s/Saas~Fee--Switzerland/homes?refinement_paths%5B%5D=%2Fhomes&date_picker_type=calendar&search_type=user_map_move&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2026-01-01&monthly_length=3&monthly_end_date=2026-04-01&price_filter_input_type=2&channel=EXPLORE&place_id=ChIJp2CD9qJFj0cRzho9eqWJgOU&acp_id=74e0fe62-22b2-4992-ab87-943645bf07b0&query=Saas-Fee%2C%20Switzerland&search_mode=regular_search&price_filter_num_nights=5&ne_lat=46.115691236231484&ne_lng=7.934395665391833&sw_lat=46.1111932114823&sw_lng=7.928865876837563&zoom=16.82584670940735&zoom_level=16.82584670940735&search_by_map=true&disable_auto_translation=true',
                 expecting: Map)
 
         then:
